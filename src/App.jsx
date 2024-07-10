1e9b1fb36416de1fe449d7e878afc516afafe7da
HuynhLongNhat <nhathuynh227@gmail.com>
2024-07-10 13:32:57 +0700

    Login Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import Chat from "./components/Chat"
 import Details from "./components/Details"
 import List from "./components/List"
-
+import Login from "./components/Login"
 
 const App = () => {
+  const user = false
   return (
     <div className='container'>
-      <List />
-      <Chat />
-      <Details />
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Details />
+        </>
+      ) : <Login />}
+
     </div>
   )
 }
